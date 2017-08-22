fcc486c9a430410eacacea2929396ace416a31f2
Diego Berrios <dberriosa@bancochile.cl>
2017-08-22 15:43:52 -0300

    se agregan imágenes múltiples a productos

--- productos/serializers.py
+++ productos/serializers.py
@@ -1,8 +1,22 @@
 from rest_framework import serializers
-from productos.models import Product
+from productos.models import Product, Image
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = ('id','product', 'owner', 'photo')
+
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    # products_images = serializers.SlugRelatedField(
+    #     many=True,
+    #     read_only=True,
+    #     slug_field='id'
+    #  )
+    products_images = ImageSerializer(
+        many=True
+     )
     class Meta:
         model = Product
-        fields = ('id', 'name', 'nick', 'quantity', 'pub_date', 'photo')
+        fields = ('id', 'name', 'nick', 'quantity','creator','products_images')
